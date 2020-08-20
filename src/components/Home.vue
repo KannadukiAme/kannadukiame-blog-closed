cb0316a7a256e751994ce80e8da1a0462d7c62f0
KannadukiAme <misuzunagisa@gmail.com>
2020-08-20 15:13:14 +0800

    feat: improve Home Page & update README

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center container mx-auto px-4 py-12">
-    <div class="text-6xl text-gray-800">正在作业中...</div>
+    <div class="text-5xl text-gray-800 animate-pulse">正在建设中...</div>
   </div>
 </template>
 
