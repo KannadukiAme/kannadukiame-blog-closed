e772b5352f8bc7167757314e96faafe8b257c475
KannadukiAme <misuzunagisa@gmail.com>
2020-09-21 13:54:51 +0800

    fix(style): font size for article's title

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto px-4 py-12">
     <div class="mb-5" v-for="(item, index) in list" :key="index">
-      <div class="text-5xl font-bold">
+      <div class="text-3xl font-bold">
         <g-link class="hover:text-sora" :to="item.node.path">{{item.node.title}}</g-link>
       </div>
       <div class="text-base text-gray-500 mb-2">发表于 {{item.node.date}}</div>
