60f499573dc0eb62c6195d19ec444b70c29f8fe6
KannadukiAme <misuzunagisa@gmail.com>
2020-09-30 10:51:54 +0800

    fix: back to top btn init hidden

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -12,6 +12,7 @@ export default {
   name: "BackToTop",
   mounted() {
     const backToTop = document.querySelector('#backToTop')
+    backToTop.style.display = "none"
     window.addEventListener('scroll', () => {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         backToTop.style.display = "block"
